b777c35cc7552c744b9c84ce04d693c0f06f4291
qi0523 <1426042225@qq.com>
2022-11-08 07:38:09 +0000

    root path

--- constant/constants.go
+++ constant/constants.go
@@ -1,7 +1,7 @@
 package constant
 
 const (
-	ContainerdRoot      = "var/lib/containerd/io.containerd.content.v1.content"
+	ContainerdRoot      = "/var/lib/containerd/io.containerd.content.v1.content"
 	ContainerdSockPath  = "/run/containerd/containerd.sock"
 	ContainerdNameSpace = "default"
 )
